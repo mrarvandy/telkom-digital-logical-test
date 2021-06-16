bbb8a4868c8f75f54e4bc12fff1e26f6cf711b0e
mrarvandy <vavandanpangan@gmail.com>
2021-06-16 13:14:03 +0700

    reinit comit

--- test/src/App.js
+++ test/src/App.js
@@ -1,19 +1,40 @@
-import Login from './views/login'
-import Home from './views/home'
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { listRepos } from 'github-apis'
+import Swal from 'sweetalert2';
 
 function App() {
+  const [ user, setUser ] = useState('owner');
+  const [ repos, setRepos ] = useState({});
+
+  useEffect(() => {
+    const options = {
+      token: process.env.REACT_APP_SECRET_KEY,
+      owner: user,
+      repo: ''
+    };
+    (async () => {
+      const data = await listRepos(options);
+      setRepos(data);
+      console.log(data);
+    })();
+  }, [user])
+
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path='/login'>
-          <Login></Login>
-        </Route>
-        <Route exact path='/'>
-          <Home></Home>
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
